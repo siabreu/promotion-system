02880bf08f394099042dace4cd445730196ecad4
Simone de Abreu <siabreu@gmail.com>
2021-03-18 17:30:19 -0300

    WIP gerar cupons

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :promotions, only: [:index, :show, :new, :create]
+  resources :promotions, only: [:index, :show, :new, :create] do
+    post 'generate_coupons', on: :member
+  end
+
 end
